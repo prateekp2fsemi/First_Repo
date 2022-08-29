21d0c0e95bcefa16df17e512b31fe1d42b40c210
prateekp2fsemi <109515960+prateekp2fsemi@users.noreply.github.com>
2022-08-29 13:01:56 +0530

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -5,3 +5,5 @@ print("New changes in Branch")
 print("New changes in Branch after emergency fix")
 
 print("New Line added to demonstrate push to GitHub")
+
+print("New line added to demonstrate branching in GitHub")
